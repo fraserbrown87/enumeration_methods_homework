6e819a195d1999fac4652f9d376347d2459007c5
fraserbrown87 <fraserbrown87@gmail.com>
2018-01-25 09:59:42 +0000

    update

--- array_tasks.js
+++ array_tasks.js
@@ -1,36 +1,62 @@
 var arrayTasks = {
 
-	// concat: function (arr1, arr2) {
+	concat: function (arr1, arr2) {
+		arr2.forEach(function(element){
+			arr1.push(element);
+		})
+		return arr1;
+	},
 
-	// },
+	insertAt: function (arr, itemToAdd, index) {
+		arr.splice(index, 0, itemToAdd);
+		return arr;
+	},
 
-	// insertAt: function (arr, itemToAdd, index) {
-		
-	// },
+	square: function (arr) {
+		var array = arr.map(function(number){
+			number ** 2
+		}
+		return array;
 
-	// square: function (arr) {
-		
-	// },
+	},
 
-	// sum: function (arr) {
-		
-	// },
+	sum: function (arr) {
+		var total = 0;
 
-	// findDuplicates: function (arr) {
-		
-	// },
+		arr.forEach(function(element) {
+			total += element;
+		})
 
-	// removeAndClone: function (arr, valueToRemove) {
-		
-	// },
+		return total;
 
-	// findIndexesOf: function (arr, itemToFind) {
-		
-	// },
+	},
 
-	// sumOfAllEvenNumbersSquared: function (arr) {
-		
-	// }
+	findDuplicates: function (arr) {
+		var duplicates = [];
+		arr.forEach(function(element){
+			for(number of arr){
+				if (number ==)
+			}
+		})
+
+	},
+
+	removeAndClone: function (arr, valueToRemove) {
+		filter
+
+	},
+
+	findIndexesOf: function (arr, itemToFind) {
+
+
+	},
+
+	sumOfAllEvenNumbersSquared: function (arr) {
+		var
+
+		filter
+
+	}
 
 }
 
